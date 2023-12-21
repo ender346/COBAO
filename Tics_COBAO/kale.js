9ff2642f253c621ec460fa84d421e924b8d701e8
Emanuel Hernández Santiago <107447125+ender346@users.noreply.github.com>
2023-12-20 22:52:47 -0600

    Update kale.js

---
+++ Tics_COBAO/kale.js
@@ -0,0 +1,10 @@
+apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev
+
+
+
+
+
+
+-
+
+git clone –recursive https://github.com/polybar/polybar
